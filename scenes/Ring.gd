bb7c520ed0ce78ba449fce2249ced29185f3e83e
Sylphiann <hydrolilium@gmail.com>
2025-03-14 19:25:01 +0700

    Finish Tutorial 3 individual tasks

---
+++ scenes/Ring.gd
@@ -0,0 +1,25 @@
+extends Area2D
+
+var velocity = Vector2(0, -100)
+var is_collected = false
+
+@onready var anim_sprite = $AnimatedSprite2D
+@onready var audio_player = $AudioStreamPlayer2D
+
+func _ready():
+	anim_sprite.play("default")
+
+
+func _on_body_entered(body: Node2D) -> void:
+	if not is_collected:
+		audio_player.play()
+	is_collected = true
+	
+func _process(delta):
+	if is_collected:
+		velocity.y += gravity * delta
+		global_position += velocity * delta
+
+		if velocity.y > 0 and global_position.y >= position.y:  
+			await audio_player.finished
+			queue_free()
